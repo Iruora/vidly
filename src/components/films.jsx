59861d2cb61a5332365386b3eee5a4d9259682b8
Iruora <arrouurii@gmail.com>
2020-03-29 16:28:45 +0100

    VIDLY- Reformat code

--- src/components/films.jsx
+++ src/components/films.jsx
@@ -1,62 +1,69 @@
-import React, { Component } from 'react';
-import * as API from '../services/fakeMovieService';
+import React, { Component } from "react";
+import { getMovies, deleteMovie } from "../services/fakeMovieService";
 class Film extends Component {
-    state = {
-        movies: [],
-    }
-    componentDidMount() {
-        const movies = API.getMovies();
-        this.setState({movies});
-    }
+  state = {
+    movies: []
+  };
+  componentDidMount() {
+    const movies = getMovies();
+    this.setState({ movies });
+  }
 
-    delete(id: number) {
-        API.deleteMovie(id);
-        // console.log(movies);
-        this.setState({movies: API.getMovies()});
-    }
-    renderTable() {
-        return (
-            <React.Fragment>
-                    <h3>Showing {this.state.movies.length} movies in the database.</h3>
-                    <table className="table">
-                    <thead>
-                        <tr>
-                        <th scope="col">Title</th>
-                        <th scope="col">Genre</th>
-                        <th scope="col">Stock</th>
-                        <th scope="col">Rate</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.movies.map(
-                            movie => 
-                                <tr key={movie._id}>
-                                    <td>{movie['title']}</td>
-                                    <td>{movie['genre']['name']}</td>
-                                    <td>{movie.numberInStock}</td>
-                                    <td>{movie.dailyRentalRate}</td>
-                                    <td>
-                                        <button 
-                                            className="btn btn-sm btn-danger"
-                                            onClick={() => this.delete(movie._id)}
-                                        >
-                                            Delete
-                                        </button>
-                                    </td>
-                                </tr>
-                        )}
-                    </tbody>
-                    </table>
-            </React.Fragment>
-        );
-    }
-    render() { 
-        return (
-            <React.Fragment>
-                {this.state.movies.length ? this.renderTable() : <span className="alert alert-warning">There are no movies in database</span>}
-            </React.Fragment>
-        );
-    }
+  handelDelete(id: number) {
+    deleteMovie(id);
+    // console.log(movies);
+    this.setState({ movies: getMovies() });
+  }
+  renderTable() {
+    const { length: count } = this.state.movies;
+    return (
+      <React.Fragment>
+        <h3>Showing {count} movies in the database.</h3>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie["title"]}</td>
+                <td>{movie["genre"]["name"]}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    className="btn btn-sm btn-danger"
+                    onClick={() => this.handelDelete(movie._id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.movies.length ? (
+          this.renderTable()
+        ) : (
+          <span className="alert alert-warning p-3 m-3">
+            There are no movies in database
+          </span>
+        )}
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default Film;
